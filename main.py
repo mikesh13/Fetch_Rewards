f1de689c310dd9ea9c5d26d249ed73bba60d3e53
mikesh13 <65006076+mikesh13@users.noreply.github.com>
2021-03-16 03:57:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Date: 3/12/2021
 
 This file contains the code for the web service to perform the comparison.
 '''
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from config import DevConfig
 from CompareText import CompareTxt
 
@@ -15,16 +15,29 @@ app.config.from_object(DevConfig)
 
 @app.route('/')
 def index():
+	'''
+	Opens index page
+	'''
 	return render_template("index.html")
 
 
 @app.route('/compare_text', methods=['GET', 'POST'])
 def compare_text():
+	'''
+	Opens the campare_text page for user to input the two texts,
+	and a number for the maximum lengh of n-grams if they want to specify.
+	'''
 	return render_template("compare_text.html")
 
 
 @app.route('/result', methods=['GET', 'POST'])
 def result():
+	'''
+	Opens the result page
+	
+	When responding to a POST request, it will perform the comparison and print the result (similarity score).
+	In other case, it will send users back to the index page.
+	'''
 	if request.method == 'POST':
 		txt1 = request.form['text1']
 		txt2 = request.form['text2']
@@ -35,7 +48,7 @@ def result():
 		len_txt2 = len(comparison.content)
 
 		if len(n) > 0 and (int(n) > len_txt1 or int(n) > len_txt2):
-			return render_template("error.html")
+			return redirect(url_for('error'))
 
 		if len(n) > 0:
 			comparison.compare(longest_n_gram=int(n))
@@ -52,6 +65,13 @@ def result():
 
 @app.route('/error')
 def error():
+	'''
+	Opens the error page.
+	When users input a number n for the n-grams that is longer than the text,
+	they will be redirected to this page to notify them about the error.
+	
+	They will be redirected to the compare_text page afterwards.
+	'''
 	return render_template("error.html")
 
 
